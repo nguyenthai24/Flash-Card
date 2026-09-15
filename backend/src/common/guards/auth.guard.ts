def980c8463cbe4b788a7da5fd7049bc74049655
nguyehthai24 <thainv@visionnetwork.vn>
2026-09-15 17:58:08 +0700

    update

--- backend/src/common/guards/auth.guard.ts
+++ backend/src/common/guards/auth.guard.ts
@@ -1,7 +1,10 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
   canActivate(context: ExecutionContext): boolean {
     return false;
   }
